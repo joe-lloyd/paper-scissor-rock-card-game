651a72922a8066ff06db3f6fc115547540d178c7
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2020-09-27 13:24:07 +0200

    added countdown font and text

---
+++ src/entities/CountDown.ts
@@ -0,0 +1,29 @@
+import * as Phaser from 'phaser';
+import Game from "../scenes/Game";
+
+class CountDownController {
+  countDownText: CountDownText;
+  constructor(scene) {
+    this.countDownText = new CountDownText(scene);
+    scene.add.existing(this.countDownText);
+  }
+
+  update() {
+    
+  }
+}
+
+class CountDownText extends Phaser.GameObjects.Text {
+  constructor(scene: Game) {
+    const style: Phaser.Types.GameObjects.Text.TextStyle = {
+      fontFamily: 'PressStart2P',
+      fontSize: "32px"
+    }
+
+    super(scene, scene.scale.width / 2, scene.scale.height / 4, "Round Start", style);
+
+    this.setOrigin(0.5, 0.5);
+  }
+}
+
+export default CountDownController;
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,10 +1,12 @@
 import { Scene } from 'phaser';
 import Cards from '../entities/Cards';
 import PlayMat from '../entities/PlayArea';
+import CountDown from '../entities/CountDown';
 
 class Game extends Scene {
     Cards: Cards;
     PlayMat: PlayMat;
+    CountDown: CountDown;
 
     constructor() {
         super({
@@ -18,8 +20,10 @@ class Game extends Scene {
     }
 
     create() {
+        this.cameras.main.backgroundColor.setTo(129, 133, 166);
         this.PlayMat = new PlayMat(this);
         this.Cards = new Cards(this);
+        this.CountDown = new CountDown(this);
     }
 
     update(time: number, delta: number) {
